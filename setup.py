99ea289650cb3d025d8ff177eb31b63c3dcd6dd3
pafapafa <te754450@gmail.com>
2026-03-22 20:44:43 +0900

    Update setup.py
    Signed-off-by: pafapafa <te754450@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='pa-battle-cats',
+    name='pa-battle_cats_save_file_generater',
     version='1.0',
     py_modules=['battle_cats_save_file_generater'], 
 )
